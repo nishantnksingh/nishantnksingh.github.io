42f5906d45dcf09d763754d10504281d198a36ec
Nishant Kumar Singh <nishantnksingh@users.noreply.github.com>
2021-10-07 04:25:28 +0530

    react second commit

--- learningreact/classcomponent_1/src/components/App.js
+++ learningreact/classcomponent_1/src/components/App.js
@@ -1,20 +1,29 @@
 import React, {Component, useState} from "react";
+import ReactDOM from 'react-dom';
 import '../styles/App.css';
-import Para from "./components/paragraph";
+
 
 class App extends Component {
     constructor(props) {
 		super(props);
+		
 	};
+	printpara() {
+		ReactDOM.render(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>, document.getElementById("root"));
+	}
 
     render() {
     	return(
     		<div id="main">
-				<button id="click"></button>
+				<button id="click" onClick={this.printpara}>Submit</button>
     		</div>
     	);
     }
 	
+	
 }
+
+console.log("app js working");
+ReactDOM.render(<App />, document.getElementById("root"));
 export default App;
 
